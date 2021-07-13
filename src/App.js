92dcf1d169cee4599e87887b458a0ec395e4cdb7
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-13 11:38:51 -0700

    deck component fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Deck from './Deck.js';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <Deck />
       </div>
     )
   }
